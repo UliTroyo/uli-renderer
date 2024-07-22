9b863823ba18c3f4e52469e95a7eae1fd99566ee
Uli Troyo <5466444+UliTroyo@users.noreply.github.com>
2024-07-21 20:15:14 -0400

    [change] Mock-up Bresenham from notes.

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,80 @@
 const std = @import("std");
 const zigimg = @import("zigimg");
-const Wiggle = @import("wiggle.zig");
+const wiggle = @import("wiggle.zig").wiggle;
 
 const VERSION = "v0.1.0";
 const WIDTH = 240;
 const HEIGHT = 240;
 const PIXELS = WIDTH * HEIGHT;
+
+fn drawLine(x0: i32, y0: i32, x1: i32, y1: i32) void {
+    if (x0 == x1) {
+        // horizontal line
+        return;
+    }
+    if (y0 == y1) {
+        // vertical line
+        return;
+    }
+
+    const dx = x1 - x0;
+    const dy = y1 - y0;
+
+    const run_longer_than_rise = @abs(dx) > @abs(dy);
+    const rise_longer_than_run = @abs(dy) > @abs(dx);
+
+    const y_decrements = dy < 0;
+    const x_decrements = dx < 0;
+
+    const wrong_y = rise_longer_than_run and y_decrements;
+    const wrong_x = run_longer_than_rise and x_decrements;
+
+    const flip = wrong_x or wrong_y;
+
+    if (flip) {
+        line(x1, y1, x0, y0);
+    } else {
+        line(x0, y0, x1, y1);
+    }
+}
+
+fn line(x0: i32, y0: i32, x1: i32, y1: i32) void {
+    const dx = x1 - x0;
+    const dy = y1 - y0;
+    const run_longer_than_rise = @abs(dy) > @abs(dx);
+
+    if (run_longer_than_rise) {
+        // we loop through y's
+        var offset = if (dx < 0) -1 else 1;
+        var threshold = (2 * dx) - dy;
+        var x = x0;
+
+        while (y < y1) {
+            // plot(x,y)
+            if (threshold > 0) {
+                x = x + offset;
+                D = D + (2 * (dx - dy));
+            } else {
+                D = D + (2 * dx);
+            }
+        }
+    } else {
+        // we loop through x's
+        var offset = if (dy < 0) -1 else 1;
+        var threshold = (2 * dy) - dx;
+        var y = y0;
+
+        while (x < x1) {
+            // plot(y,x)
+            if (threshold > 0) {
+                y = y + offset;
+                D = D + (2 * (dy - dx));
+            } else {
+                D = D + (2 * dy);
+            }
+        }
+    }
+}
 
 pub fn main() !void {
     var image_buffer: [WIDTH * HEIGHT * 4]u8 = undefined;
@@ -16,20 +85,21 @@ pub fn main() !void {
     var image = try zigimg.Image.create(allocator, WIDTH, HEIGHT, .rgba32);
     defer image.deinit();
 
-    // src.pixel_format.PixelFormat.rgba32
-    // const format = zigimg.Image.pixelFormat(image);
-    const green = zigimg.Colors(zigimg.color.Rgba32).Green;
-    const black = zigimg.Colors(zigimg.color.Rgba32).Black;
+    {
+        // src.pixel_format.PixelFormat.rgba32
+        // const format = zigimg.Image.pixelFormat(image);
+        const green = zigimg.Colors(zigimg.color.Rgba32).Green;
+        const black = zigimg.Colors(zigimg.color.Rgba32).Black;
 
-    for (0..PIXELS) |i| {
-        image.pixels.rgba32[i] = black;
+        for (0..PIXELS) |i| {
+            image.pixels.rgba32[i] = black;
+        }
+        image.pixels.rgba32[28920] = green;
+        image.pixels.rgba32[28921] = green;
     }
-    image.pixels.rgba32[28920] = green;
-    image.pixels.rgba32[28921] = green;
 
-    const wiggle = Wiggle.from(std.time.milliTimestamp());
-
-    const filename = wiggle ++ "-" ++ VERSION ++ ".png";
+    const hash = wiggle(std.time.milliTimestamp());
+    const filename = "images/" ++ hash ++ "-" ++ VERSION ++ ".png";
 
     try image.writeToFilePath(filename, .{ .png = .{} });
 }
--- src/wiggle.zig
+++ src/wiggle.zig
@@ -3,16 +3,13 @@ const std = @import("std");
 const digits = [_]u8{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 const base22 = [_]u8{ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 const base32 = digits ++ base22;
+const jan_1_2020 = 1577836800000;
 
-fn dateHash(now: i64) i64 {
-    const jan_1_2020 = 1577836800000;
-    return (now - jan_1_2020);
-}
-
-pub fn from(timestamp: i64) [6]u8 {
+pub fn wiggle(timestamp: i64) [6]u8 {
+    const date: u64 = @intCast(@divFloor((timestamp - jan_1_2020), 1000));
     var arr: [6]u8 = undefined;
     var cycles: u8 = 0;
-    var remaining_seconds: u64 = @intCast(@divFloor(dateHash(timestamp), 1000));
+    var remaining_seconds: u64 = date;
 
     while (remaining_seconds > 0 and cycles < 6) {
         if (cycles < 4) {
